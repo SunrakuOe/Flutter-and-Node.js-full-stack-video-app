270da343de6cd8b1b5547a7145abe1235dc9f064
Sunraku <logoms796@gmail.com>
2026-07-10 13:04:33 +0530

    connect to mongo atlas

---
+++ backend/src/db/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import { DB_NAME } from '../constants.js'
+import dns from 'dns'
+
+const connectDB = async () => {
+    try {
+        dns.setServers(['8.8.8.8'])
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\nMongoDB connected | DB HOST : ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB connection error", error)
+        process.exit(1)
+    }
+}                  
+
+export default connectDB
