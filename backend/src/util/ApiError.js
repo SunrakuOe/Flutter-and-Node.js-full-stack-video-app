d04dc813da8867118746dfa54df2949549350220
Sunraku <logoms796@gmail.com>
2026-07-14 15:19:08 +0530

    Util code

---
+++ backend/src/util/ApiError.js
@@ -0,0 +1,24 @@
+//TODO: Lern more bout this Error in documents from node
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong!",
+        errors = [],
+        stack = ""
+    ) {
+        super(message)
+        this.statusCode = statusCode
+        this.data = null //TODO: assignment - learn about this.data
+        this.message
+        this.success = false;
+        this.errors = errors
+
+        if(stack){
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export {ApiError}
