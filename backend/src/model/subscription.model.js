8d63444b456d873d8f35e80f5a3f6be74ec6a844
Sunraku <logoms796@gmail.com>
2026-08-07 15:54:28 +0530

    Add subscription model

---
+++ backend/src/model/subscription.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+    {
+        subscriber: {
+            // The one who is subscribing
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+        channel: {
+            // To whom the subscriber is subscribing
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Subscription = mongoose.Model("Subscription", subscriptionSchema);
