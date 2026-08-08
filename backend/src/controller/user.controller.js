7464466657e6eaa9bbccbaa3d2ddbb81fb115e60
Sunraku <logoms796@gmail.com>
2026-08-08 09:14:22 +0530

    fix(use controller): fix creation of the ApiResponse object

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -298,7 +298,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 
     return res
         .status(200)
-        .json(new new ApiResponse(200, {}, "password changed successfully")());
+        .json(new ApiResponse(200, {}, "password changed successfully")());
 });
 
 const getCurrentUser = asyncHandler(async (req, res) => {
