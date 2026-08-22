8e670235c699c288b1f958a5bb4dbe357b112a42
Sunraku <logoms796@gmail.com>
2026-08-22 17:41:39 +0530

    feat(video route): add get route for the getAllVideos controller

--- backend/src/route/video.route.js
+++ backend/src/route/video.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
     deleteVideo,
+    getAllVideos,
     getVideoById,
     publishAVideo,
     togglePublishStatus,
@@ -11,18 +12,22 @@ import { upload } from "../middleware/multer.middleware.js";
 import {
     authorizeVideoOwnership,
     checkAuthorizeToView,
+    checkIsOwner,
 } from "../middleware/video.middleware.js";
 
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/").post(
-    upload.fields([
-        { name: "videoFile", maxCount: 1 },
-        { name: "thumbnail", maxCount: 1 },
-    ]),
-    publishAVideo
-);
+router
+    .route("/")
+    .get(checkIsOwner, getAllVideos)
+    .post(
+        upload.fields([
+            { name: "videoFile", maxCount: 1 },
+            { name: "thumbnail", maxCount: 1 },
+        ]),
+        publishAVideo
+    );
 
 router
     .route("/:videoId")
