a408e7469e97ac0c162834b6a64a8e401acb988c
Sunraku <logoms796@gmail.com>
2026-08-25 16:25:39 +0530

    feat(tweet.controller): add getUserTweets controller to get all tweets of an user

--- backend/src/controller/tweet.controller.js
+++ backend/src/controller/tweet.controller.js
@@ -2,6 +2,8 @@ import { asyncHandler } from "../util/asyncHandler.js";
 import { ApiError } from "../util/ApiError.js";
 import { ApiResponse } from "../util/ApiResponse.js";
 import { Tweet } from "../model/tweet.model.js";
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../model/user.model.js";
 
 const createTweet = asyncHandler(async (req, res) => {
     /* 
@@ -22,7 +24,84 @@ const createTweet = asyncHandler(async (req, res) => {
         owner: req.user?._id,
     });
 
-    return res.status(200).json(new ApiResponse(200, tweet, "tweet created successfully"));
+    return res
+        .status(200)
+        .json(new ApiResponse(200, tweet, "tweet created successfully"));
 });
 
-export {createTweet}
+const getUserTweets = asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+
+    if(!userId && !isValidObjectId(userId)){
+        throw new ApiError(400, "invalid userId")
+    }
+
+    const user = await User.findById(userId).select("_id userName avatar.url fullName");
+
+    if(!user){
+        throw new ApiError(400, "user not available")
+    }
+
+    const tweets = await Tweet.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId),
+            },
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "parent",
+                as: "likes",
+                pipeline: [
+                    {
+                        // NOTE: I think this match check is unnecessery because the ObjectId in mongoDB is unique across your entire database, not just your collection
+                        $match: {
+                            parentModel: "Tweet",
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $lookup: {
+                from: "comments",
+                localField: "_id",
+                foreignField: "parent",
+                as: "comments",
+                pipeline: [
+                    {
+                        $match: {
+                            parentModel: "Comment",
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                likesCount: {
+                    $size: "$likes",
+                },
+                commentsCount: {
+                    $size: "$comments",
+                },
+            },
+        },
+        {
+            $project: {
+                _id: 1,
+                content: 1,
+                likesCount: 1,
+                commentsCount: 1,
+            }
+        }
+    ]);
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {user, tweets}, "tweets fetched successfully"));
+});
+
+export { createTweet, getUserTweets };
