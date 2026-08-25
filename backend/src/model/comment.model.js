ba1b208954257ad531d6aaf72f505343708fa96e
Sunraku <logoms796@gmail.com>
2026-08-25 16:24:17 +0530

    feat(comment.model): add comment model

---
+++ backend/src/model/comment.model.js
@@ -0,0 +1,22 @@
+import { Schema } from "mongoose";
+
+const commentSchema = new Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    parent: {
+        type: Schema.Types.ObjectId,
+        refPath: "parentModel",
+        required: true,
+    },
+    parentModel: {
+        type: String,
+        required: true,
+        enum: ["Video", "Tweet", "Comment"],
+    },
+});
