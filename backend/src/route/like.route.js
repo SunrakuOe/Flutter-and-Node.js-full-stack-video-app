45d811fa8297f5d082b32a4dd5d6a39f3ea0a0c6
Sunraku <logoms796@gmail.com>
2026-08-27 10:48:23 +0530

    feat(like.route): add routes for toggling comment like and tweet like and get all liked videos of currnnt user

--- backend/src/route/like.route.js
+++ backend/src/route/like.route.js
@@ -1,5 +1,5 @@
 import {Router} from "express" 
-import { toggleVideoLike } from "../controller/like.controller.js"
+import { toggleVideoLike, toggleCommentLike, toggleTweetLike, getLikedVideos } from "../controller/like.controller.js"
 import { verifyJWT } from "../middleware/auth.middleware.js"
 
 const router = Router()
@@ -7,5 +7,11 @@ const router = Router()
 router.use(verifyJWT)
 
 router.route("/toggle/video/:videoId").post(toggleVideoLike)
+
+router.route("/toggle/comment/:commentId").post(toggleCommentLike)
+
+router.route("/toggle/tweet/:tweetId").post(toggleTweetLike)
+
+router.route("/video").get(getLikedVideos)
 
 export default router
