4450c38ccc99e90ef91a1a5f52b2cd1bd72ffe18
Sunraku <logoms796@gmail.com>
2026-08-27 10:49:03 +0530

    fix(comment.model): fix export

--- backend/src/model/comment.model.js
+++ backend/src/model/comment.model.js
@@ -21,4 +21,4 @@ const commentSchema = new Schema({
     },
 });
 
-export default Comment = mongoose.model("Comment", commentSchema);
+export const Comment = mongoose.model("Comment", commentSchema);
