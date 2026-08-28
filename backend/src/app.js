47e900347d6a07b92f70df24f949c02952d8e06e
Sunraku <logoms796@gmail.com>
2026-08-28 18:03:10 +0530

    feat(app): add comment routes to lcomment endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -40,6 +40,10 @@ import likeRouter from "./route/like.route.js";
 
 app.use("/api/v1/like", likeRouter); 
 
+import commentRouter from "./route/comment.route.js"
+
+app.use("/api/v1/comment", commentRouter); 
+
 //NOTE: add error handling middleware always at the end because this handler only get called by Express if any next(any argument and not necesserily an error) called before the handler
 // remember that the error-handling-middleware (a middleware with (err, req, res, next)) get called if the next(err) get called in any nearest(no any other error-handling-middleware in between) middleware that is attached before the error-handling middleware
 // if there is not any user defined error-handling middleware then the default error-handling middleware of Express will get called
