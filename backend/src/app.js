6d4c9721f4eff720ce7f8af004028fc9ec7cfa9a
Sunraku <logoms796@gmail.com>
2026-09-02 22:21:12 +0530

    feat(dashboard.route): add dashboard route to the application

--- backend/src/app.js
+++ backend/src/app.js
@@ -52,6 +52,10 @@ import healthcheckRouter from "./route/healthcheck.route.js";
 
 app.use("/api/v1/healthcheck", healthcheckRouter);
 
+import dashboardRouter from "./route/dashboard.route.js";
+
+app.use("/api/v1/dashboard", dashboardRouter);
+
 //NOTE: add error handling middleware always at the end because this handler only get called by Express if any next(any argument and not necesserily an error) called before the handler
 // remember that the error-handling-middleware (a middleware with (err, req, res, next)) get called if the next(err) get called in any nearest(no any other error-handling-middleware in between) middleware that is attached before the error-handling middleware
 // if there is not any user defined error-handling middleware then the default error-handling middleware of Express will get called
